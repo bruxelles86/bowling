743fd32618ea58bfa40235f9d6041f4750b678e5
bruxelles86 <w_gant@yahoo.co.uk>
2017-05-14 17:27:19 +0100

    Correctly scores strikes

--- spec/ScoreUpdaterSpec.js
+++ spec/ScoreUpdaterSpec.js
@@ -15,9 +15,30 @@ describe("ScoreUpdater", function() {
     expect(game.frames[0].finalFrameScore).toEqual(15)
   });
 
+  it("gives frame 4 a score of 20 if a spare, and the next ball is a strike", function() {
+    game.frames[5].startingBallIndex = 6; game.frames[5].isSpare = true;
+    game.ballPins = [1, 2, 3, 4, 5, 5, 6, 4, 10]
+    game.updateScores(game.ballPins);
+    expect(game.frames[5].finalFrameScore).toEqual(20)
+  });
+
   it("gives frame 1 a score of 17 if a strike, and total of 7 scored over next two balls", function() {
     game.frames[0].startingBallIndex = 0; game.frames[0].isStrike = true; game.ballPins = [10, 5, 2];
     game.updateScores(game.ballPins);
     expect(game.frames[0].finalFrameScore).toEqual(17)
+  });
+
+  it("gives frame 3 a score of 22 if a strike, and the next two balls are a strike and a two", function() {
+    game.frames[2].startingBallIndex = 4; game.frames[2].isStrike = true;
+    game.ballPins = [4, 3, 6, 8, 10, 10, 2];
+    game.updateScores(game.ballPins);
+    expect(game.frames[2].finalFrameScore).toEqual(22)
+  });
+
+  it("gives frame 1 a score of 30 if a strike, and the next two balls are both strikes", function() {
+    game.frames[0].startingBallIndex = 0; game.frames[0].isStrike = true;
+    game.ballPins = [10, 10, 10];
+    game.updateScores(game.ballPins);
+    expect(game.frames[0].finalFrameScore).toEqual(30)
   });
 });
--- src/ScoreUpdater.js
+++ src/ScoreUpdater.js
@@ -1,14 +1,14 @@
-// This will iterate through the frames, and where any are 'finished', marked as either strike
-// or spare, but with a final score of 0:
-// if 'spare', check whether array's [frame.startingBallNumber + 2] index exists. If so, set
-// final score equal to this plus the two ball scores (i.e. 10)
-// if 'strike', check whether frame.startingBallNumber + 1 *and* +2 both exist. Adjust score as above.
-
 scoreUpdater = function(ballPinsArray) {
 
   this.frames.forEach(function(frame) {
     if(frame.isSpare && ballPinsArray[frame.startingBallIndex + 2] != undefined) {
       frame.finalFrameScore = 10 + ballPinsArray[frame.startingBallIndex + 2]
+    } else if(frame.isStrike
+              && ballPinsArray[frame.startingBallIndex + 1] != undefined
+              && ballPinsArray[frame.startingBallIndex + 2] != undefined)
+
+              {frame.finalFrameScore = 10 + ballPinsArray[frame.startingBallIndex + 1]
+                + ballPinsArray[frame.startingBallIndex + 2]
     }
   });
 };
